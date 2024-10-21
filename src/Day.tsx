f2eadc157f9356eac442a2d70a043ef87e045e93
LTstrange <835422774@qq.com>
2024-10-21 20:59:55 +0800

    Change icon

--- src/Day.tsx
+++ src/Day.tsx
@@ -1,8 +1,8 @@
 function Day({ date, highlight }: { date: Date; highlight: boolean }) {
   return (
     <div
-      className={`bg-white w-10 h-10 rounded flex justify-center items-center ${
-        highlight ? "bg-slate-700 text-slate-100" : ""
+      className={`w-10 h-10 rounded flex justify-center items-center ${
+        highlight ? "bg-slate-700 text-slate-100" : "bg-white"
       }`}
       style={{ gridColumnStart: date.getDay() + 1 }}
     >
